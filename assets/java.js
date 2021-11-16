575737943c8b5f6f88dd5791182c828aad8d7a04
Paper Girl <jamie.rose.528@gmail.com>
2021-11-15 22:53:13 -0800

    let the button start the timer and fixed timer

--- assets/java.js
+++ assets/java.js
@@ -1,20 +1,25 @@
 var timeEl = document.querySelector(".timer");
-var startButton = document.querySelector("#start")
-var secondsLeft = 60;
+var startButton = document.querySelector("#start");
+var secondsLeft = 5;
+var onGoing = false
+
+startButton.addEventListener("click", setTime)
 
 function setTime() {
-
+if(onGoing) {
+    return
+}
+onGoing = true
   var timerInterval = setInterval(function() {
     secondsLeft--;
     timeEl.textContent = secondsLeft;
 
     if(secondsLeft === 0) {
-
-      clearInterval(timerInterval);
-      sendMessage();
+    timeEl.style.backgroundColor="none" 
+    onGoing = false
+    clearInterval(timerInterval);
     }
+  timeEl.style.backgroundColor="red"  
 
   }, 1000);
 }
-
-setTime()
